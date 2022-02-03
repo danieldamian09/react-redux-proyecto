13bb67620bcfc2d2ac98a50d1a3404fbb0576f28
danieldamian09 <humbertorivero9@gmail.com>
2022-02-03 18:47:21 -0300

    creating thunk method and updating reducers

--- src/redux/reducers/login.js
+++ src/redux/reducers/login.js
@@ -1,10 +1,34 @@
-import {createReducer} from "@reduxjs/toolkit"
-import { checkLogin } from "../actions/login";
+import {createReducer} from "@reduxjs/toolkit";
+import {checkingAuth, completedAuth, errorAuth} from "../actions/login";
 
-const loginReducer = createReducer([], builder => {
-  builder.addCase(checkLogin.toString() , (state, action) => {
-    console.log(action)
-  })
-})
+// ! en el action viene el type y el payload
+//! creo esta bandera isChekingAuth 
+
+const loginReducer = createReducer([], (builder) => {
+	builder
+		.addCase(checkingAuth.toString(), (state, action) => {
+      // aca creo la bandera isChekingAuth
+			return {
+        ...state,
+        isChekingAuth: true,
+      }
+		})
+		.addCase(completedAuth.toString(), (state, action) => {
+			return {
+        ...state, 
+        isAuth: action.payload.isAuth,
+        isChekingAuth: false,
+      }
+		})
+		.addCase(errorAuth.toString(), (state, action) => {
+			return {
+        ...state,
+        isAuth: false,
+        isChekingAuth: false,
+        error: action.payload.error
+
+      }
+		});
+});
 
 export default loginReducer;
