cb0841e741a02981525176a7ad49e4ecb4f7c85c
danieldamian09 <humbertorivero9@gmail.com>
2022-02-04 20:19:18 -0300

    creating the shortcuts for the selector

--- src/redux/selector.js
+++ src/redux/selector.js
@@ -1,4 +1,6 @@
-// creamos como accesos directos a estas piezas de informacion
+// creamos como accesos directos a estas piezas de informacion para utilizarlas en nuestros selectores
 export const isAuthSel = state => state.loginReducer.isAuth;
-
 export const isChekingAuthSel = state => state.loginReducer.isCheckingAuth;
+
+export const isSendingAuthFormSel = state => state.loginReducer.isSendingAuthForm
+export const isSuccessLoggedSel = state =>  state.loginReducer.isSuccessLogged
