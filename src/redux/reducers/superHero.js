7775ced438d2c54553ab52a8a6368ee8c3cc084f
danieldamian09 <humbertorivero9@gmail.com>
2022-02-05 15:49:59 -0300

    create reducers and import actions

---
+++ src/redux/reducers/superHero.js
@@ -0,0 +1,49 @@
+import {createReducer} from "@reduxjs/toolkit";
+
+//* importamos las acciones (3)
+import {
+	startFetchingSuperheroes,
+	successFetchingSuperheroes,
+	errorFetchingSuperheroes,
+} from "../actions/superHero";
+
+//* Declaramos el state inicial(estas nos van a servir como banderas en el componente)
+const initialState = {
+	isfectchingSuperhero: false,
+	error: undefined,
+	superheroes: [],
+};
+
+//* Declaramos el reducer (creamos los casos)
+const superHeroesReducer = createReducer(initialState, (builder) => {
+	builder
+		.addCase(startFetchingSuperheroes.toString(), (state, action) => {
+			return {
+				...state,
+				isfectchingSuperhero: true,
+				error: undefined,
+			};
+		})
+		.addCase(successFetchingSuperheroes.toString(), (state, action) => {
+			return {
+				...state,
+				isfectchingSuperhero: false,
+				superheroes: action.payload.data,
+			};
+		})
+		.addCase(errorFetchingSuperheroes.toString(), (state, action) => {
+			return {
+				...state,
+				isfectchingSuperhero: false,
+				superheroes: [],
+				error: action.payload.error,
+			};
+		})
+		.addDefaultCase((state, action) => {
+			return {
+				state,
+			};
+		});
+});
+
+export default superHeroesReducer;
