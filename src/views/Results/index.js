b86690a720ca01146ab76c61aad79a521f2c437f
danieldamian09 <humbertorivero9@gmail.com>
2022-02-05 16:31:50 -0300

    import the selectors to the component superHeroes

--- src/views/Results/index.js
+++ src/views/Results/index.js
@@ -1,5 +1,5 @@
-import { useEffect } from "react";
-import { useParams } from "react-router-dom";
+import {useEffect} from "react";
+import {useParams} from "react-router-dom";
 import ErrorComponent from "./components/ErrorComponent";
 import ResultsList from "./components/ResultsList";
 import NoResults from "./components/NoResults";
@@ -7,30 +7,41 @@ import Header from "../../components/Header";
 import Spinner from "../../components/Spinner";
 
 //* para Redux
-import { useDispatch } from "react-redux";
-import { fetchSuperheroes } from "../../redux/actions/superHero";
+import {useDispatch} from "react-redux";
+import {fetchSuperheroes} from "../../redux/actions/superHero";
+import {useSelector, shallowEqual} from "react-redux";
+//* los selectores
+import {
+	isFectchingSuperheroSel,
+	superHeroesErrorSel,
+	superheroesSel,
+} from "../../redux/selector";
 
 export default function Results() {
-  const { searchText } = useParams();
+	const {searchText} = useParams();
 
-  //* instanciar useDispatch
-  const dispatch = useDispatch()
+	//* instanciar useDispatch
+	const dispatch = useDispatch();
+	//* nuestras banderas que viene de los selectores
+	const isFectchingSuperhero = useSelector(isFectchingSuperheroSel, shallowEqual);
+  const superHeroesError = useSelector(superHeroesErrorSel, shallowEqual)
+  const superHeroes = useSelector(superheroesSel, shallowEqual)
 
-  //* en el useEffect disparo mi actions que va a hacer la llamada a la API por medio del thunk
-  useEffect(() => {
-    dispatch(fetchSuperheroes(searchText))
-  }, []);
+	//* en el useEffect disparo mi actions que va a hacer la llamada a la API por medio del thunk
+	useEffect(() => {
+		dispatch(fetchSuperheroes(searchText));
+	}, []);
 
-  return (
-    <div>
-      <Header />
-      <div className="px-3 pb-2 mt-12">
-        <h2 className="text-xl font-bold">Resultados para: {searchText}</h2>
-        {/* {isLoading && <Spinner />}
+	return (
+		<div>
+			<Header />
+			<div className="px-3 pb-2 mt-12">
+				<h2 className="text-xl font-bold">Resultados para: {searchText}</h2>
+				{/* {isLoading && <Spinner />}
         <ErrorComponent error={error} />
         <ResultsList data={results} />
         {!isLoading && !results?.length && <NoResults />} */}
-      </div>
-    </div>
-  );
+			</div>
+		</div>
+	);
 }
