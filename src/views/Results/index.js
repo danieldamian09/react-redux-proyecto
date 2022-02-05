8923a1edc84b01e38efed09a9ed0e8c227d5c209
danieldamian09 <humbertorivero9@gmail.com>
2022-02-04 22:09:09 -0300

    triggering the action that comes from the thunk fetchSuperheroes and passing the parameter to search from the component

--- src/views/Results/index.js
+++ src/views/Results/index.js
@@ -6,15 +6,17 @@ import NoResults from "./components/NoResults";
 import Header from "../../components/Header";
 import Spinner from "../../components/Spinner";
 
+import { useDispatch } from "react-redux";
+import { fetchSuperheroes } from "../../redux/actions/superHero";
+
 export default function Results() {
   const { searchText } = useParams();
 
-  
-
- 
+  // redux
+  const dispatch = useDispatch()
 
   useEffect(() => {
-    fetchResultsRef.current()?.catch(null);
+    dispatch(fetchSuperheroes(searchText))
   }, []);
 
   return (
@@ -22,10 +24,10 @@ export default function Results() {
       <Header />
       <div className="px-3 pb-2 mt-12">
         <h2 className="text-xl font-bold">Resultados para: {searchText}</h2>
-        {isLoading && <Spinner />}
+        {/* {isLoading && <Spinner />}
         <ErrorComponent error={error} />
         <ResultsList data={results} />
-        {!isLoading && !results?.length && <NoResults />}
+        {!isLoading && !results?.length && <NoResults />} */}
       </div>
     </div>
   );
